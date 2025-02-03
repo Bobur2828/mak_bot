83fb99f40960e395194ee3407de9111b26d81b9e
IntelComputers <farmusic@inbox.ru>
2025-02-03 21:46:17 +0500

    serv

---
+++ config.py
@@ -0,0 +1,7 @@
+SQLALCHEMY_URL="sqlite+aiosqlite:///db.sqlite3"
+TOKEN="8069084800:AAEanAwGOJobkKse2Kh5jk4vpihFalRMq7M"
+ADMIN=[1327096215,5923735368]
+CHANNEL_ID = ['https://t.me/fargenius_group']
+
+
+# CHANNEL_ID = ['-1002161315623']
